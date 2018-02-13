1a609decd51db1bec45464df1ee3e6befb4e4d4b
yasufumi sekine <yasufumi_sekine@r.recruit.co.jp>
2018-02-13 18:52:37 +0900

    made by Spring Initializr

---
+++ spring-boot-admin-server/src/main/java/jp/oretama/springbootadminserver/SpringBootAdminServerApplication.java
@@ -0,0 +1,12 @@
+package jp.oretama.springbootadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);
+	}
+}
